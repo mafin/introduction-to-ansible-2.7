5f68a04c0929bcb4a289e084bae713cb5da69bbf
Petr Enin <petr.enin@gmail.com>
2022-02-02 18:52:03 +0100

    update tf rules

--- tf/main.tf
+++ tf/main.tf
@@ -53,4 +53,23 @@ resource "aws_security_group" "instance" {
   tags = {
     Name = "allow-ssh"
   }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "ws_1_public_ip" {
+  value = aws_instance.ansible-webserver[0].public_ip
+}
+
+output "ws_2_public_ip" {
+  value = aws_instance.ansible-webserver[1].public_ip
+}
+
+output "lb_public_ip" {
+  value = aws_instance.ansible-loadbalancer.public_ip
 }
